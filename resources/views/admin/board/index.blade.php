8004c313a224032d11e6aef647ee6b683ba1d202
lilnemesis505 <nmewcople@gmail.com>
2025-12-14 10:18:42 +0700

    wd

--- resources/views/admin/board/index.blade.php
+++ resources/views/admin/board/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="d-flex justify-content-between align-items-center mb-4">
-    <h2><i class="bi bi-layers-fill"></i> จัดการหน้าผู้ถือหุ้น (Board)</h2>
+    <h2><i class="bi bi-layers-fill"></i> จัดการหน้าสวัสดิการ</h2>
     <a href="{{ route('admin.board.create') }}" class="btn btn-success shadow-sm">
         <i class="bi bi-plus-lg"></i> เพิ่มหัวข้อใหม่
     </a>
